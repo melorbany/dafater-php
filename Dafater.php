27f89369e94baa1b83f7a84d7c8d9051a172cb9d
Mohamed Elorbany <m.elorbany@ymail.com>
2020-03-10 10:21:20 +0300

    Update Dafater Document

--- Dafater.php
+++ Dafater.php
@@ -1,8 +1,8 @@
 <?php
 
-define("CLIENT_ID", "apitest");
-define("CLIENT_SECRET", "12345678");
-define("AUTHENTICATION", "https://dafateridentity-test.azurewebsites.net/connect/token");
+define("CLIENT_ID", "mybusiness-z6n9ob");
+define("CLIENT_SECRET", "GDv55ZBnnt");
+define("AUTHENTICATION", "https://identity.dafater.biz/connect/token");
 
 /**
  * Class Dafater
@@ -46,22 +46,14 @@ Class Dafater
      * @param array $filters
      * @return mixed
      */
-    public function getDocument($document_type ,array $filters = [])
+    public function getDocument($document_type ,array $params = [])
     {
 
         $url = "https://api.dafater.biz/document/$document_type";
 
+        $url = str_replace(' ','%20',$url);
 
-        foreach ($filters as $param => $val){
-            $url .= $param.'='.$val.'&';
-        }
-        $url = rtrim($url, "/?");
-        $url = rtrim($url, "&");
-//        $url = str_replace(' ','%20',$url);
-
-        var_dump($url);
-
-        $obj = json_decode($this->get($url, true));
+        $obj = json_decode($this->get($url,$params, true));
         return $obj;
     }
 
@@ -76,6 +68,22 @@ Class Dafater
 
         $fields["doctype"] = $document_type;
         $obj = json_decode($this->post("https://api.dafater.biz/document",
+            $fields, true, true));
+
+        return $obj;
+    }
+
+
+    /**
+     * @param $document_type
+     * @param $fields
+     * @return mixed
+     */
+    public function updateDocument($document_type, $fields)
+    {
+
+        $fields["doctype"] = $document_type;
+        $obj = json_decode($this->put("https://api.dafater.biz/document",
             $fields, true, true));
 
         return $obj;
@@ -144,10 +152,11 @@ Class Dafater
      * @param bool $auth
      * @return bool|string
      */
-    public function get($url, $auth = false)
+    public function get($url,$params =[] , $auth = false)
     {
 
         $ch = curl_init();
+
         curl_setopt($ch, CURLOPT_URL, $url);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 
@@ -165,6 +174,65 @@ Class Dafater
 
         return $result;
     }
+
+
+    /**
+     * @param $url
+     * @param $fields
+     * @param bool $auth
+     * @param bool $json
+     * @return mixed
+     */
+    public function put($url, $fields, $auth = false, $json = false)
+    {
+
+        $header = [
+            'Accept: application/json',
+        ];
+
+        if ($json) {
+
+            $fields_string = json_encode($fields);
+            $header[] = 'Content-Type: application/json';
+            $header[] = 'Content-Length: ' . strlen($fields_string);
+
+        } else {
+            $fields_string = "";
+            foreach ($fields as $key => $value) {
+                $fields_string .= $key . '=' . $value . '&';
+            }
+            rtrim($fields_string, '&');
+        }
+
+        if ($auth && $this->access_token && $this->token_type) {
+
+            $header[] = 'Accept: application/json';
+            $header[] = 'Authorization:' . $this->token_type . ' ' . $this->access_token;
+        }
+
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+        curl_setopt($ch, CURLOPT_POST, count($fields));
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $fields_string);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_REFERER, 1);
+
+        if (count($header) > 0) {
+            curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
+        }
+
+
+        $result = curl_exec($ch);
+
+        if ($result === false) {
+            echo 'Curl error: ' . curl_error($ch);
+        }
+        curl_close($ch);
+
+        return $result;
+    }
+
 }
 
 ?>
